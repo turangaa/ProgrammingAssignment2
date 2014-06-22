75a424c4c02559a46e0d0fbc9a9eae4515b86278
turangaa <natasakalioud@Natasas-MacBook-Air.local>
2014-06-22 14:49:17 +0300

    This is my attempt for the Programming Assignment of the Coursera R Programming class, that caches the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The following two functions, can save us time in calculating the inverse of a matrix, if the inverse is cached and the matrix has not changed. For convenience, we assume the input is always a square invertible matrix, so no check is being performed 
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix creates a special "matrix" object that can cache its inverse. The function is then used in the second function to convert the input to the right "matrix" object.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInv <- function(solve) m <<- solve
+  getInv <- function() m
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+  
+  
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the given matrix, after converting it to the special "matrix" object created with the abov function. The function first checks if inverse has been caclulated and skips computation. Otherwise, calculates inverse with solve() function and sets the value in the cache via the setInv function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- makeCacheMatrix(x)$getInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- makeCacheMatrix(x)$get()
+  m <- solve(data, ...)
+  makeCacheMatrix(x)$setInv(m)
+  m
+  
 }
+
+
+
